09848f566520d15e27b6700ae620fdb4d9b15750
PlatonovaEkaterina <85198835+PlatonovaEkaterina@users.noreply.github.com>
2022-10-03 01:53:10 +0300

    Add files via upload

---
+++ ApiSwagger/database/~vs2EAB.sql
@@ -0,0 +1,35 @@
+INSERT INTO Otdel(Otdel_ID, Name, Telephone) VALUES (1, 'Первый', '+79234567890');
+INSERT INTO Otdel(Otdel_ID, Name, Telephone) VALUES (2, 'Второй', '+7678901234');
+INSERT INTO Otdel(Otdel_ID, Name, Telephone) VALUES (3, 'Третий', '+7567540123');
+INSERT INTO Otdel(Otdel_ID, Name, Telephone) VALUES (4, 'Четвертый', '+7532890123');
+
+INSERT INTO Education(Education_ID, Name) VALUES (1, 'Высшее');
+INSERT INTO Education(Education_ID, Name) VALUES (2, 'Среднее');
+INSERT INTO Education(Education_ID, Name) VALUES (3, 'Неоконченное высшее');
+
+INSERT INTO Position(Position_ID, PositionName_ID, Oklad, Status) VALUES (1, 1, 23000, 0);
+INSERT INTO Position(Position_ID, PositionName_ID, Oklad, Status) VALUES (2, 1, 40000, 0);
+INSERT INTO Position(Position_ID, PositionName_ID, Oklad, Status) VALUES (3, 2, 40000, 1);
+INSERT INTO Position(Position_ID, PositionName_ID, Oklad, Status) VALUES (4, 3, 60000, 1);
+INSERT INTO Position(Position_ID, PositionName_ID, Oklad, Status) VALUES (5, 4, 45000, 1);
+INSERT INTO Position(Position_ID, PositionName_ID, Oklad, Status) VALUES (6, 5, 15000, 0);
+INSERT INTO Position(Position_ID, PositionName_ID, Oklad, Status) VALUES (7, 3, 50000, 1);
+INSERT INTO Position(Position_ID, PositionName_ID, Oklad, Status) VALUES (8, 2, 78000, 0);
+INSERT INTO Position(Position_ID, PositionName_ID, Oklad, Status) VALUES (9, 4, 25000, 1);
+INSERT INTO Position(Position_ID, PositionName_ID, Oklad, Status) VALUES (10, 6, 70000, 0);
+INSERT INTO Position(Position_ID, PositionName_ID, Oklad, Status) VALUES (11, 1, 100000,1);
+INSERT INTO Position(Position_ID, PositionName_ID, Oklad, Status) VALUES (12, 2, 10000, 0);
+INSERT INTO Position(Position_ID, PositionName_ID, Oklad, Status) VALUES (13, 7, 60000, 1);
+INSERT INTO Position(Position_ID, PositionName_ID, Oklad, Status) VALUES (14, 8, 35000, 1);
+
+
+
+INSERT INTO Sotrud(Sotrud_ID, Otdel_ID, Position_ID, Education_ID, F_Name,L_Name) VALUES (1, 1, 1, 1, 'Иван',' Иванов');
+INSERT INTO Sotrud(Sotrud_ID, Otdel_ID, Position_ID, Education_ID, F_Name,L_Name) VALUES (2, 2, 2, 2, 'Иван',' Федоров');
+INSERT INTO Sotrud(Sotrud_ID, Otdel_ID, Position_ID,  Education_ID, F_Name,L_Name) VALUES (3, 3, 3, 1, 'Дмитрий',' Иванов');
+INSERT INTO Sotrud(Sotrud_ID, Otdel_ID, Position_ID,  Education_ID, F_Name,L_Name) VALUES (4, 4, 4, 2, 'Александр',' Фомов');
+INSERT INTO Sotrud(Sotrud_ID, Otdel_ID, Position_ID,  Education_ID, F_Name,L_Name) VALUES (5, 1, 5, 2, 'Федор',' Булкин');
+INSERT INTO Sotrud(Sotrud_ID, Otdel_ID, Position_ID, Education_ID, F_Name,L_Name) VALUES (6, 2, 6, 3, 'Владислав',' Гусь');
+INSERT INTO Sotrud(Sotrud_ID, Otdel_ID, Position_ID,  Education_ID, F_Name,L_Name) VALUES (7, 3, 7, 3, 'Евгений',' Дулкин');
+INSERT INTO Sotrud(Sotrud_ID, Otdel_ID, Position_ID, Education_ID, F_Name,L_Name) VALUES (8, 4, 8, 3,'Анастасия',' Уткина');
+INSERT INTO Sotrud(Sotrud_ID, Otdel_ID, Position_ID,  Education_ID, F_Name,L_Name) VALUES (9, 1, 9, 1, 'Даниил',' Лебедев');
